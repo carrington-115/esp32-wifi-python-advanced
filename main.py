1ef307d1616d57e4dc3efcfb10671971667ff195
Mark Carrington <frumarkcarrington@gmail.com>
2023-09-22 13:31:10 +0530

    added a function that uses the internal capabilities of the pin function from the machine library

--- main.py
+++ main.py
@@ -46,15 +46,30 @@ def blink_with_ms(t):
     sleep_ms(t) # you can do the same for microseconds by using sleep_us()
     led.value(0)
 
+# this function is a function that shows all the functions available in the pin
+def pin_functions():
+    # creating a pin in the program
+    pin = Pin(2, Pin.OUT) # Using Pin this way because I destructure it on calling machine at the top of the program.and
+    pin.on() # the on() function can be used to set the pin high
 
+    pin.off() # the off() can be used to set the pin low    
+
+def blinK_sketch_with_abstract_functions():
+    # pin = Pin(2, Pin.OUT)
+    # we can still using the pin.value(1 or 0) in the program to set the value of the pin
+    pin = Pin(2, Pin.OUT, value=1) # on creating the pin, it is possible to set the value of the pin. e.g. the value here has been set to 1 that is equivalent to high
+    pin.on()
+    sleep_ms(2000)
+    pin.off()
 
 # this is the section to run the program
 while True:
     print("The code has started running this proof you are in the main program of the code\n")
     # connect_to_wifi_STA_MODE() # This was the connect in STA MOD
     # actAsAccessPoint() # this is the function that show how to make the esp32 an access point other devices can connect to
-    blink_with_ms(500)
-     
+    # blink_with_ms(500) # this test was a test for blink the led in ms
+    blinK_sketch_with_abstract_functions() # this is the sketch that test the pin abstract function of the machine library.and
+
         
 
 # import time
